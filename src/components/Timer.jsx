dffb0d7cc4c389dfa4131fb041faa1de7d6a34d7
Nuwan Dhanushka Welihinda <96749237+Nuwandw98@users.noreply.github.com>
2022-08-07 03:43:20 +0530

    initial commit

---
+++ src/components/Timer.jsx
@@ -0,0 +1,26 @@
+/** @format */
+
+import React, { useState } from "react";
+import { useEffect } from "react";
+
+const Timer = ({ setTimeOut, questionNumber }) => {
+  const [timer, setTimer] = useState(30);
+
+  useEffect(() => {
+    if (timer === 0) {
+      return setTimeOut(true);
+    }
+    const interval = setInterval(() => {
+      setTimer((prev) => prev - 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [setTimeOut, timer]);
+
+  useEffect(() => {
+    setTimer(30);
+  }, [questionNumber]);
+
+  return timer;
+};
+
+export default Timer;
